45eadb5c2b77b129e3b1f5cee0100c42d404ba62
Arik Jones <arik@Macintosh-2.local>
2008-11-12 00:31:43 -0600

    Updated the readme. Modded the functionality to parse YAML and allow a default filter to be set. Uses some code from the original (first) default_page_parts extension by Andrea Franz of GravityBlast.

--- default_page_parts_extension.rb
+++ default_page_parts_extension.rb
@@ -1,18 +1,30 @@
 class DefaultPagePartsExtension < Radiant::Extension
-  version "0.1"
+  version "0.2"
   description "Enables auto-creation of default page parts for a page's children"
   url "http://github.com/santry/radiant-default-page-parts-extension"
 
   Page.class_eval do
     alias :original_parent_equals :parent=
-
-    def parent=(parent)
-      default_parts = parent.render_part('default_child_page_parts').strip.split(/\s*,\s*/)
-      default_parts.each do |name|
-        self.parts << PagePart.new(:name => name, :filter_id => Radiant::Config['defaults.page.filter'])
+    
+    def parent=(parent)  
+      string = parent.render_part("page_part_config")
+      unless string.empty?
+        config = YAML::load(string)
+      else
+        config = {}
+      end
+      default_filter = config["filter"].to_s.camelize
+      filter = ["SmartyPants", "Markdown", "Textile"].include?(default_filter) ? "#{default_filter}" : nil
+      if config["parts"].is_a?(Array) && config["parts"].size > 0
+        self.parts = []
+        config["parts"].each do |name|
+          self.parts << PagePart.new(:name => name, :filter_id => filter)
+        end
       end
       original_parent_equals(parent)
     end
+    
+    
   end
 
 end
