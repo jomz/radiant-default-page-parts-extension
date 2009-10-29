b38e3d8531536250a793b6383f732e478a7a3f0a
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-10-29 15:54:58 -0400

    Updated to work with radiant 0.8.1. Now monkey patches Admin::PagesController method new and monkey patch adds new_with_page_parts method to Page

--- default_page_parts_extension.rb
+++ default_page_parts_extension.rb
@@ -1,29 +1,46 @@
 class DefaultPagePartsExtension < Radiant::Extension
-  version "0.2"
+  version "0.3"
   description "Enables auto-creation of default page parts for a page's children"
-  url "http://github.com/santry/radiant-default-page-parts-extension"
-
-  Page.class_eval do
-    alias :original_parent_equals :parent=
+  url "http://github.com/ckirby/radiant-default-page-parts-extension"
+  
+  Admin::PagesController.class_eval do
+    alias :original_new :new
     
-    def parent=(parent)  
-      string = parent.render_part("page_part_config")
-      unless string.empty?
-        config = YAML::load(string)
+    def new
+      unless params[:page_id].blank?
+        parent_page = Page.find(params[:page_id]);
+        child_parts = parent_page.render_part("page_part_config")
+        self.model = model_class.new_with_page_parts(config, child_parts)
       else
-        config = {}
+        self.model = model_class.new_with_defaults(config)
+        self.model.slug = '/'
       end
-      if config.is_a?(Array) && config.size > 0
-        self.parts = []
-        config.each do |part|
-          default_filter = part['filter'].to_s.camelize
-          filter = ["SmartyPants", "Markdown", "Textile"].include?(default_filter) ? "#{default_filter}" : nil
-          name = part['name']
-          self.parts << PagePart.new(:name => name, :filter_id => filter)
-        end
-      end
-      original_parent_equals(parent)
+      response_for :singular
     end
   end
-
+  
+  class << Page
+    def new_with_page_parts(config = Radiant::Config, parts = String)
+      begin
+        unless parts.empty?
+          config = YAML::load(parts)
+          log.debug(config)
+          if config.is_a?(Array) && config.size > 0
+            page = new
+            config.each do |part|
+              default_filter = part['filter'].to_s.camelize
+              filter = ["SmartyPants", "Markdown", "Textile"].include?(default_filter) ? "#{default_filter}" : nil
+              name = part['name']
+              page.parts << PagePart.new(:name => name, :filter_id => filter)
+            end
+            page
+          end
+        else
+          new_with_defaults(config)
+        end
+      rescue
+        new_with_defaults(config)
+      end
+    end
+  end
 end
